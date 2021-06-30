121d2a793f32c032df0af6215924542ffe1fd62b
CauveryRaja <cauveryraja25@gmail.com>
2021-06-30 10:05:45 +0530

    Add testcase to check toggleCollapse invokation

--- react-app/src/components/accordion/collapsibleHeader/CollapsibleHeader.test.js
+++ react-app/src/components/accordion/collapsibleHeader/CollapsibleHeader.test.js
@@ -1,6 +1,8 @@
 import { render, fireEvent } from '@testing-library/react';
 import CollapsibleHeader from './CollapsibleHeader';
 
+const mockToggleCollapse = jest.fn();
+
 describe('<CollapsibleHeader />', () => {
     it('should render Collapsible Header component', () => {
         render(<CollapsibleHeader />);
@@ -22,17 +24,20 @@ describe('<CollapsibleHeader />', () => {
     });
 
     it('should toggle expand and collapse on clicking them', async () => {
-        const { queryByTestId, getByTestId }  = render(<CollapsibleHeader title="Header 1" expanded={true}/>);
+        const { queryByTestId, getByTestId }  = render(<CollapsibleHeader title="Header 1" expanded={true} 
+            toggleCollapse={mockToggleCollapse}/>);
         const expandBtn = queryByTestId('expand-btn');
         const collapseBtn = getByTestId('collapse-btn');
 
         await fireEvent.click(collapseBtn);
 
+        expect(mockToggleCollapse).toHaveBeenCalledTimes(1);
         expect(expandBtn).toBeDefined();
         expect(collapseBtn).toBeNull();
 
         await fireEvent.click(expandBtn);
 
+        expect(mockToggleCollapse).toHaveBeenCalledTimes(2);
         expect(expandBtn).toBeNull();
         expect(collapseBtn).toBeDefined();
     });
