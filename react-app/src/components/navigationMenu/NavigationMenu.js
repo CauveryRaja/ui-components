1e776244e87bf3367c93f15cb21480b8b742ac34
CauveryRaja <cauveryraja25@gmail.com>
2021-07-07 20:22:19 +0530

    Create Menu component for nested menus

--- react-app/src/components/navigationMenu/NavigationMenu.js
+++ react-app/src/components/navigationMenu/NavigationMenu.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import Menu from './'
 
 class NavigationMenu extends Component {
     render() {
         return <ul>
         {
             this.props.menuList.map(elm => {
-                return <li key={elm.id} data-testid={elm.id}></li>
+                switch(elm.type) {
+                    case 'item': return <li key={elm.id} data-testid={elm.id}></li>
+                    case 'list': return 
+                }
             })
         }
         </ul>
--- react-app/src/components/navigationMenu/NavigationMenu.test.js
+++ react-app/src/components/navigationMenu/NavigationMenu.test.js
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react';
 import NavigationMenu from './NavigationMenu';
-import { flatMenus } from './mocks/NavigationMenu.data';
+import { flatMenus, nestedMenus } from './mocks/NavigationMenu.data';
 
 describe('NavigationMenu', () => {
     it('Should render Navigation Menu', () => {
@@ -12,4 +12,12 @@ describe('NavigationMenu', () => {
         expect(getByTestId('item-0')).toBeDefined();
         expect(getByTestId('item-1')).toBeDefined();
     });
+
+    it('Should render all nested menus', () => {
+        const { getByTestId } = render(<NavigationMenu menuList={nestedMenus}/>);
+        expect(getByTestId('item-0')).toBeDefined();
+        expect(getByTestId('item-1')).toBeDefined();
+        expect(getByTestId('menu-0')).toBeDefined();
+        expect(getByTestId('menu-0.item-0')).toBeDefined();
+    })
 });
