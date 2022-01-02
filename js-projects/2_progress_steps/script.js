a773d643733343acac18426e5c39c0859cb18dba
CauveryRaja <cauveryraja25@gmail.com>
2022-01-02 13:37:30 +0530

    Update animated stepper component

--- js-projects/2_progress_steps/script.js
+++ js-projects/2_progress_steps/script.js
@@ -2,17 +2,38 @@ let currIndex = 0;
 
 const indicators = document.getElementsByClassName('screen-indicator');
 const progressElm = document.getElementsByClassName('progress')[0];
+const prevBtn = document.getElementById('prev-btn');
+const nextBtn = document.getElementById('next-btn');
+
+disableControls();
 
 function previous() {
+    // Delay should not be applied when removing class
+    indicators[currIndex].style.transitionDelay = '0s';
     indicators[currIndex].classList.remove('completed');
     --currIndex;
     progressElm.style.width = `${currIndex/(indicators.length-1)*100}%`;
-    console.log('width', progressElm.style.width);
+    disableControls();
 }
 
 function next() {
     ++currIndex;
+    // Delay should be applied when adding class
+    indicators[currIndex].style.transitionDelay = '0.6s';
     indicators[currIndex].classList.add('completed');
     progressElm.style.width = `${currIndex/(indicators.length-1)*100}%`;
-    console.log('width', progressElm.style.width);
+    disableControls();
+}
+
+function disableControls() {
+    if(currIndex <= 0) {
+        prevBtn.disabled = true;
+    }
+    else if(currIndex >= indicators.length-1) {
+        nextBtn.disabled = true;
+    }
+    else {
+        prevBtn.disabled = false;
+        nextBtn.disabled = false;
+    }
 }
