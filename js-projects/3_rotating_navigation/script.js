49d948ebb70eb7645505f839dee89dd1917165ab
CauveryRaja <cauveryraja25@gmail.com>
2022-01-12 07:34:07 +0530

    Animate menu items in rotating nav

--- js-projects/3_rotating_navigation/script.js
+++ js-projects/3_rotating_navigation/script.js
@@ -1,15 +1,18 @@
 const page = document.getElementsByClassName('page')[0];
 const menuBtn = document.getElementsByClassName('menu-btn')[0];
 const closeBtn = document.getElementsByClassName('close-btn')[0];
+const menu = document.getElementsByClassName('menu')[0];
 
 menuBtn.addEventListener('click', () => {
     page.classList.add('active');
     menuBtn.classList.add('rotate');
     closeBtn.classList.add('rotate');
+    menu.classList.add('showMenus');
 });
 
 closeBtn.addEventListener('click', () => {
     page.classList.remove('active');
     menuBtn.classList.remove('rotate');
     closeBtn.classList.remove('rotate');
+    menu.classList.remove('showMenus');
 });
