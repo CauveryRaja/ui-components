038aefe9c3fd5c2024d20cfe4c01719420e8137b
CauveryRaja <cauveryraja25@gmail.com>
2022-02-16 08:08:19 +0530

    Update timeline layout

--- js-projects/responsive_timeline/script.js
+++ js-projects/responsive_timeline/script.js
@@ -1,10 +1,16 @@
-let indicators = Array.from(document.getElementsByClassName('indicator'));
-let contents = Array.from(document.getElementsByClassName('content'));
+// let indicators = Array.from(document.getElementsByClassName('indicator'));
+// let contents = Array.from(document.getElementsByClassName('content'));
 
-indicators.forEach((elm, index) => {
-    elm.style.top = `${15 * (index + 1)}%`; 
+// indicators.forEach((elm, index) => {
+//     elm.style.top = `${15 * (index + 1)}%`; 
+// });
+
+// contents.forEach((elm, index) => {
+//     elm.style.top = `${15 * (index + 1)}%`;
+// })
+
+let entries = Array.from(document.getElementsByClassName('entry'));
+
+entries.forEach((elm, index) => {
+    // elm.style.top = `${15 * (index + 1)}%`; 
 });
-
-contents.forEach((elm, index) => {
-    elm.style.top = `${15 * (index + 1)}%`;
-})
