c33c5e108e6a83502108269d36ba96140e5723e3
CauveryRaja <cauveryraja25@gmail.com>
2022-02-23 08:18:40 +0530

    Understanding document scroll behaviour

--- js-projects/virtualised_feed/script.js
+++ js-projects/virtualised_feed/script.js
@@ -4,9 +4,13 @@ init();
 
 let entries = Array.from(container.children);
 
-document.addEventListener('scroll', (event) => {
+window.addEventListener('scroll', (event) => {
     // let entryRect = document.getElementById('entry-0').getBoundingClientRect();
     // console.log(event, entryRect.top);
+
+    let windowRect = document.getElementsByClassName('bar-top')[0].getBoundingClientRect();
+    console.log(event.scrollTop, windowRect.left);
+
     checkFirstElement();
 });
 
@@ -34,3 +38,19 @@ function getElementWithIndex(index) {
                 <footer></footer>
             </div>`;
 }
+
+
+// Detecting Scroll direction - yet to use
+
+var lastScrollTop = 0;
+
+// element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+element.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
+   var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+   if (st > lastScrollTop){
+      // downscroll code
+   } else {
+      // upscroll code
+   }
+   lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+}, false);
