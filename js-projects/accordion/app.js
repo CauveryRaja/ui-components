b3c6357707d8aee115671b52e10bfb2c2314fe17
CauveryRaja <cauveryraja25@gmail.com>
2022-03-27 12:10:50 +0530

    Try out animating collapsible using height property

--- js-projects/accordion/app.js
+++ js-projects/accordion/app.js
@@ -1,5 +1,5 @@
 let collapsibleHeader = document.getElementsByClassName('collapsible-header')[0];
 
 collapsibleHeader.addEventListener('click', () => {
-
+    collapsibleHeader.parentElement.classList.toggle('open');
 });
