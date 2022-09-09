0d40efc7db85e03e9dd7a44494c073e626699b55
CauveryRaja <cauveryraja25@gmail.com>
2022-09-09 08:51:43 +0530

    Move template to Todo View

--- js-projects/todo-app/src/view/Todo.js
+++ js-projects/todo-app/src/view/Todo.js
@@ -1,6 +1,18 @@
 class Todo {
     constructor(controller) {
         this.controller = controller;
+        this.todoList = document.getElementById('todo-list');
+    }
+
+    // Think how to render list of todos...
+    render(todo) {
+        return `
+            <li class="todo-item">
+                <input type="checkbox" name="check" id="check">
+                <p class="todo-title">${todo.title}</p>
+                <button class="delete-todo">X</button>
+            </li>
+        `
     }
 
     update() {
