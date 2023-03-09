dbda9cb960af9ebe279d831fa336c3b76b7e9b2a
CauveryRaja <cauveryraja25@gmail.com>
2023-03-09 15:03:35 +0530

    Use prettier to format files

--- react-tasks/src/games/tic-tac-toe/Board/Board.jsx
+++ react-tasks/src/games/tic-tac-toe/Board/Board.jsx
@@ -1,16 +1,19 @@
 import { useState } from "react";
 import Box from "../Box";
-import './Board.css'
+import "./Board.css";
 
 const Board = ({ boxValues, updateBoxValue, highlightedBoxes }) => {
-    return <div className="board">
-        {boxValues.map((value, index) => (
-                <Box value={value} 
-                    highlight={highlightedBoxes.includes(index)} 
-                    clickListener={() => updateBoxValue(index)}/>
-            )
-        )}
+  return (
+    <div className="board">
+      {boxValues.map((value, index) => (
+        <Box
+          value={value}
+          highlight={highlightedBoxes.includes(index)}
+          clickListener={() => updateBoxValue(index)}
+        />
+      ))}
     </div>
-}
+  );
+};
 
 export default Board;
